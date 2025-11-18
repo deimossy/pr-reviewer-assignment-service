73c1ed91ce37dd702d207b8e538c2b5355ddf814
deimossy <deimossy@student.21-school.ru>
2025-11-18 14:47:40 +0300

    feat(pkg/errors): add error constants from OpenAPI-generated dto

---
+++ pkg/errors/errors.go
@@ -0,0 +1,18 @@
+package errors
+
+const (
+	ErrPRNotFound      = "PR_NOT_FOUND"
+	ErrPRAlreadyMerged = "PR_MERGED"
+	ErrPRAlreadyExists = "PR_EXISTS"
+	ErrNoCandidate     = "NO_CANDIDATE"
+	ErrNotAssigned     = "NOT_ASSIGNED"
+)
+
+const (
+	ErrTeamAlreadyExists = "TEAM_EXISTS"
+	ErrTeamNotFound      = "TEAM_NOT_FOUND"
+)
+
+const (
+	ErrUserNotFound = "USER_NOT_FOUND"
+)
