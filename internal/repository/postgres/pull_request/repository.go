a6fb10f8e257ea1776cf729786f2a3fc9ded459e
deimossy <deimossy@student.21-school.ru>
2025-11-18 14:55:05 +0300

    feat(repository/pull_request): add mock implementation with TODO for PullRequestRepository interface

--- internal/repository/postgres/pull_request/repository.go
+++ internal/repository/postgres/pull_request/repository.go
@@ -1,1 +1,51 @@
-package pull_request
+package prrepo
+
+import (
+	"context"
+	"github.com/deimossy/pr-reviewer-assignment-service/internal/models"
+	"github.com/deimossy/pr-reviewer-assignment-service/internal/repository"
+	"github.com/jmoiron/sqlx"
+)
+
+type pullRequestRepository struct {
+	db *sqlx.DB
+}
+
+func NewPullRequestRepository(db *sqlx.DB) repository.PullRequestRepository {
+	return &pullRequestRepository{db: db}
+}
+
+func (r *pullRequestRepository) Create(ctx context.Context, pr models.PullRequest) error {
+	// TODO: implement
+	return nil
+}
+
+func (r *pullRequestRepository) Merge(ctx context.Context, prID string) (*models.PullRequest, error) {
+	// TODO: implement
+	return nil, nil
+}
+
+func (r *pullRequestRepository) GetByID(ctx context.Context, prID string) (*models.PullRequest, error) {
+	// TODO: implement
+	return nil, nil
+}
+
+func (r *pullRequestRepository) ListByUser(ctx context.Context, userID string) ([]models.PullRequestShort, error) {
+	// TODO: implement
+	return nil, nil
+}
+
+func (r *pullRequestRepository) AssignReview(ctx context.Context, prID string, reviewerIDs []string) error {
+	// TODO: implement
+	return nil
+}
+
+func (r *pullRequestRepository) ReplaceReview(ctx context.Context, prID string, oldReviewerID string, newReviewerID string) error {
+	// TODO: implement
+	return nil
+}
+
+func (r *pullRequestRepository) GetReviewers(ctx context.Context, prID string) ([]string, error) {
+	// TODO: implement
+	return nil, nil
+}
