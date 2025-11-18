83d67272edee8797658d0c7d6c03f474efd6bb0c
deimossy <deimossy@student.21-school.ru>
2025-11-18 14:56:21 +0300

    feat(repository/team): add mock implementation with TODO for TeamRepository interface

--- internal/repository/postgres/team/repository.go
+++ internal/repository/postgres/team/repository.go
@@ -1,1 +1,26 @@
-package team
+package teamrepo
+
+import (
+	"context"
+	"github.com/deimossy/pr-reviewer-assignment-service/internal/models"
+	"github.com/deimossy/pr-reviewer-assignment-service/internal/repository"
+	"github.com/jmoiron/sqlx"
+)
+
+type teamRepository struct {
+	db *sqlx.DB
+}
+
+func NewTeamRepository(db *sqlx.DB) repository.TeamRepository {
+	return &teamRepository{db: db}
+}
+
+func (r *teamRepository) Create(ctx context.Context, team models.Team) error {
+	// TODO: implement
+	return nil
+}
+
+func (r *teamRepository) GetByName(ctx context.Context, name string) (*models.Team, error) {
+	// TODO: implement
+	return nil, nil
+}
