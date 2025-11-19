a00b5fa1f8c9dd29e0a08db72fa5c9c4eb818c61
deimossy <deimossy@student.21-school.ru>
2025-11-19 21:45:08 +0300

    feat(repository/postgres): add queries for db from userRepository

--- internal/repository/postgres/user/queries.go
+++ internal/repository/postgres/user/queries.go
@@ -1,1 +1,27 @@
-package user
+package userrepo
+
+const (
+	createUserQuery = `
+	INSERT INTO users (user_id, username, team_name, is_active)
+	VALUES ($1, $2, $3, $4)
+	ON CONFLICT (user_id) DO NOTHING;
+	`
+
+	setActiveQuery = `
+	UPDATE users
+	SET is_active = $2
+	WHERE user_id = $1;
+	`
+
+	getByIDQuery = `
+	SELECT user_id, username, team_name, is_active
+	FROM users
+	WHERE user_id = $1;
+	`
+
+	getTeamMembersQuery = `
+	SELECT user_id, username, team_name, is_active
+	FROM users
+	WHERE team_name = $1;
+	`
+)
