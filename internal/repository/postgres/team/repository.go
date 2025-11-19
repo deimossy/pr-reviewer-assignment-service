6ca91dc4a79afce11bd7ca07907d1d063294921a
deimossy <deimossy@student.21-school.ru>
2025-11-19 21:44:48 +0300

    feat(repository/postgres): add implementation of methods for teamRepository

--- internal/repository/postgres/team/repository.go
+++ internal/repository/postgres/team/repository.go
@@ -2,8 +2,13 @@ package teamrepo
 
 import (
 	"context"
+	"database/sql"
+	"errors"
+	pgHelpers "github.com/deimossy/pr-reviewer-assignment-service/pkg/postgres"
+
 	"github.com/deimossy/pr-reviewer-assignment-service/internal/models"
 	"github.com/deimossy/pr-reviewer-assignment-service/internal/repository"
+	customErrors "github.com/deimossy/pr-reviewer-assignment-service/pkg/errors"
 	"github.com/jmoiron/sqlx"
 )
 
@@ -16,11 +21,20 @@ func NewTeamRepository(db *sqlx.DB) repository.TeamRepository {
 }
 
 func (r *teamRepository) Create(ctx context.Context, team models.Team) error {
-	// TODO: implement
+	_, err := r.db.ExecContext(ctx, createTeamQuery, team.TeamName)
+	if err != nil {
+		return pgHelpers.CheckUnique(err, "team")
+	}
 	return nil
 }
 
 func (r *teamRepository) GetByName(ctx context.Context, name string) (*models.Team, error) {
-	// TODO: implement
-	return nil, nil
+	var t models.Team
+	if err := r.db.GetContext(ctx, &t, getByNameQuery, name); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, customErrors.ErrTeamNotFound
+		}
+		return nil, err
+	}
+	return &t, nil
 }
