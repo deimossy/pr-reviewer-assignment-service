103e357409ac35ce6739c668efc94c9051d9a32e
deimossy <deimossy@student.21-school.ru>
2025-11-19 21:48:46 +0300

    feat(internal/service): add implementation of TeamService

--- internal/service/team.go
+++ internal/service/team.go
@@ -2,9 +2,9 @@ package service
 
 import (
 	"context"
-
 	"github.com/deimossy/pr-reviewer-assignment-service/internal/models"
 	"github.com/deimossy/pr-reviewer-assignment-service/internal/repository"
+	"go.uber.org/zap"
 )
 
 type TeamService interface {
@@ -13,17 +13,59 @@ type TeamService interface {
 }
 
 type teamService struct {
-	repo repository.TeamRepository
+	teamRepo repository.TeamRepository
+	userSvc  UserService
+	logger   *zap.Logger
 }
 
-func NewTeamService(repo repository.TeamRepository) TeamService {
-	return &teamService{repo: repo}
+func NewTeamService(
+	teamRepo repository.TeamRepository,
+	userSvc UserService,
+	logger *zap.Logger,
+) TeamService {
+	return &teamService{
+		teamRepo: teamRepo,
+		userSvc:  userSvc,
+		logger:   logger,
+	}
 }
 
 func (s *teamService) Create(ctx context.Context, team *models.Team) error {
-	return s.repo.Create(ctx, *team)
+	if err := s.teamRepo.Create(ctx, *team); err != nil {
+		return err
+	}
+
+	for _, m := range team.Members {
+		user := &models.User{
+			UserId:   m.UserId,
+			Username: m.Username,
+			TeamName: team.TeamName,
+			IsActive: m.IsActive,
+		}
+		if err := s.userSvc.Create(ctx, user); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func (s *teamService) GetByName(ctx context.Context, name string) (*models.Team, error) {
-	return s.repo.GetByName(ctx, name)
+	team, err := s.teamRepo.GetByName(ctx, name)
+	if err != nil {
+		return nil, err
+	}
+
+	users, err := s.userSvc.GetTeamMembers(ctx, name)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, u := range users {
+		team.Members = append(team.Members, models.TeamMember{
+			UserId:   u.UserId,
+			Username: u.Username,
+			IsActive: u.IsActive,
+		})
+	}
+	return team, nil
 }
