52b14147a5011a0b371b0f7992c466bef28575ce
deimossy <deimossy@student.21-school.ru>
2025-11-19 21:18:39 +0300

    refactor(pkg/errors): convert error string constants to error variables

--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -1,18 +1,15 @@
 package errors
 
-const (
-	ErrPRNotFound      = "PR_NOT_FOUND"
-	ErrPRAlreadyMerged = "PR_MERGED"
-	ErrPRAlreadyExists = "PR_EXISTS"
-	ErrNoCandidate     = "NO_CANDIDATE"
-	ErrNotAssigned     = "NOT_ASSIGNED"
-)
+import "errors"
 
-const (
-	ErrTeamAlreadyExists = "TEAM_EXISTS"
-	ErrTeamNotFound      = "TEAM_NOT_FOUND"
-)
+var (
+	ErrPRNotFound      = errors.New("PR_NOT_FOUND")
+	ErrPRAlreadyMerged = errors.New("PR_MERGED")
+	ErrPRAlreadyExists = errors.New("PR_EXISTS")
+	ErrNoCandidate     = errors.New("NO_CANDIDATE")
+	ErrNotAssigned     = errors.New("NOT_ASSIGNED")
 
-const (
-	ErrUserNotFound = "USER_NOT_FOUND"
+	ErrTeamAlreadyExists = errors.New("TEAM_EXISTS")
+	ErrTeamNotFound      = errors.New("TEAM_NOT_FOUND")
+	ErrUserNotFound      = errors.New("USER_NOT_FOUND")
 )
