36f381a3baef1601dad04c01a44ff7bd94cd2aa0
deimossy <deimossy@student.21-school.ru>
2025-11-19 21:43:23 +0300

    feat(repository/postgres): add queries for db from pullRequestRepository

--- internal/repository/postgres/pull_request/queries.go
+++ internal/repository/postgres/pull_request/queries.go
@@ -1,1 +1,43 @@
-package pull_request
+package prrepo
+
+const (
+	createPRQuery = `
+	INSERT INTO pull_requests (pull_request_id, pull_request_name, author_id, status, created_at)
+	VALUES ($1, $2, $3, $4, $5);
+	`
+
+	getPRByIDQuery = `
+	SELECT pull_request_id, pull_request_name, author_id, status, created_at, merged_at
+	FROM pull_requests
+	WHERE pull_request_id = $1;
+	`
+
+	listPRsByUserQuery = `
+	SELECT pull_request_id, pull_request_name, author_id, status
+	FROM pull_requests
+	WHERE author_id = $1;
+	`
+
+	updatePRStatusQuery = `
+	UPDATE pull_requests
+	SET status = $2, merged_at = $3
+	WHERE pull_request_id = $1;
+	`
+
+	insertReviewerQuery = `
+	INSERT INTO pull_request_reviewers (pull_request_id, reviewer_id)
+	VALUES ($1, $2)
+	ON CONFLICT (pull_request_id, reviewer_id) DO NOTHING;
+	`
+
+	deleteReviewerQuery = `
+	DELETE FROM pull_request_reviewers
+	WHERE pull_request_id = $1 AND reviewer_id = $2;
+	`
+
+	getReviewersQuery = `
+	SELECT reviewer_id
+	FROM pull_request_reviewers
+	WHERE pull_request_id = $1;
+	`
+)
