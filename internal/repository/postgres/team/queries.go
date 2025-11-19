b84d708f380361703a0f35b84e84949663d485f8
deimossy <deimossy@student.21-school.ru>
2025-11-19 21:44:23 +0300

    feat(repository/postgres): add queries for db from teamRepository

--- internal/repository/postgres/team/queries.go
+++ internal/repository/postgres/team/queries.go
@@ -1,1 +1,14 @@
-package team
+package teamrepo
+
+const (
+	createTeamQuery = `
+	INSERT INTO teams (team_name)
+	VALUES ($1)
+	`
+
+	getByNameQuery = `
+	SELECT team_name
+	FROM teams
+	WHERE team_name = $1;
+	`
+)
