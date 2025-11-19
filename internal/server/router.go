124d61bf13de542cb0909f47596c150505a9b8f3
deimossy <deimossy@student.21-school.ru>
2025-11-19 21:45:38 +0300

    refactor(internal/server): refactor code style

--- internal/server/router.go
+++ internal/server/router.go
@@ -1,1 +1,21 @@
 package server
+
+import (
+	"github.com/deimossy/pr-reviewer-assignment-service/internal/handlers"
+	"net/http"
+)
+
+func NewRouter(userHandler *handlers.UserHandler, teamHandler *handlers.TeamHandler, prHandler *handlers.PullRequestHandler) http.Handler {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/users/setIsActive", userHandler.SetIsActive)
+	mux.HandleFunc("/users/getReview", userHandler.GetReview)
+
+	mux.HandleFunc("/team/add", teamHandler.AddTeam)
+	mux.HandleFunc("/team/get", teamHandler.GetTeam)
+
+	mux.HandleFunc("/pullRequest/create", prHandler.CreatePR)
+	mux.HandleFunc("/pullRequest/merge", prHandler.MergePR)
+	mux.HandleFunc("/pullRequest/reassign", prHandler.ReassignReview)
+	return mux
+}
