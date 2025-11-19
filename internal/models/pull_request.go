d9f7bbd612ff62009c72f3e05890ebebf1c28b19
deimossy <deimossy@student.21-school.ru>
2025-11-19 21:35:05 +0300

    refactor(internal/models): refactor code style

--- internal/models/pull_request.go
+++ internal/models/pull_request.go
@@ -7,7 +7,7 @@ type PullRequest struct {
 	PullRequestName   string     `db:"pull_request_name"`
 	AuthorId          string     `db:"author_id"`
 	Status            string     `db:"status"`
-	CreatedAt         time.Time  `db:"created_at"`
+	CreatedAt         *time.Time `db:"created_at"`
 	MergedAt          *time.Time `db:"merged_at,omitempty"`
 	AssignedReviewers []string   `db:"-"`
 }
